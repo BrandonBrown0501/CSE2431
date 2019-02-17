e3548d9ab93672d963b602561f609049b6cd9783
brown.5562 <brown.5562@cse-std9.coeit.osu.edu>
2019-02-17 12:18:04 -0500

    Created Sudoku validator file

---
+++ SudokuValidator.java
@@ -0,0 +1,23 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ * 
+ *
+ **/
+package Sudoku;
+
+public class SudokuValidator {
+
+    private static final int THREAD_COUNT = 27;
+
+    public static void main(String[] args){
+	System.out.println("This is a valid Sudoku solution");
+    }
+
+}
+
+
+
