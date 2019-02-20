07cf0cba23e45d2ba3c74c24d2adf1f0ae950d32
Mike Gargasz <gargasz.13@osu.edu>
2019-02-20 14:50:03 -0500

    Add files via upload
    Changed error reporting & added condition for correct solution.

--- SudokuValidator.java
+++ SudokuValidator.java
@@ -1,14 +1,9 @@
 /**
- *
- *
- *
- *
- *
  * 
+ * CSE 2431 Lab 2
+ * Mike Gargasz & Brandon Brown
  *
  **/
-package Sudoku;
-
 import java.util.Vector;
 import java.lang.Integer;
 import java.util.Scanner;
@@ -16,16 +11,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SudokuValidator {
-    private static final int THREAD_COUNT = 9;
+    private static final int THREAD_COUNT = 27;
     private static final int ROW_COLUMN_LENGTH = 9;
     private static final int VALID_SUM = 45;
     private static final int SQUARE_LENGTH = 3;
 
-
-
     public static void main(String[] args) {
         Vector<Vector<Integer>> puzzle = new Vector<>();
         Scanner scanner = new Scanner(System.in);
+        Vector<Integer> errors = new Vector<>();
 
         //read in puzzle
         for (int i = 0; i < ROW_COLUMN_LENGTH; i++) {
@@ -42,34 +36,39 @@ public class SudokuValidator {
 
         //create thread pool
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
-        for(int i = 0; i < ROW_COLUMN_LENGTH; i++) {
-            executor.execute(new Rows(i, puzzle));
-            executor.execute(new Columns(i, puzzle));
-            executor.execute(new Squares(i, puzzle));
-        }
 
+        for(int i = 0; i < ROW_COLUMN_LENGTH; i++) {
+            executor.execute(new Rows(i, puzzle, errors));
+            executor.execute(new Columns(i, puzzle, errors));
+            executor.execute(new Squares(i, puzzle, errors));
+        }
         executor.shutdown();
+
+        if(errors.size() == 0) {
+            System.out.println("This solution is correct!");
+        } else {
+            System.out.println("This solution is not correct and there are " + errors.size() + " errors. Please review the above messages for insight.");
+        }
     }
 
     // The task for checking the rows of numbers
     public static class Rows implements Runnable {
         private Vector<Vector<Integer>> sudoku; // The puzzle being tested
-        private int row; // The square being tested
-        private Rows(int rowNumber, Vector<Vector<Integer>> puzzle) {
+        private int row; // The row being tested
+        private Vector<Integer> errors; // Any errors caught
+        private Rows(int rowNumber, Vector<Vector<Integer>> puzzle, Vector<Integer> numErrors) {
             sudoku = puzzle;
             row = rowNumber;
+            errors = numErrors;
         }
-
         @Override
-        /** Override the run() method to tell the system
-         *  what the task to perform
-         */
         public void run() {
             int sum = 0;
             for (int i = 0; i < ROW_COLUMN_LENGTH; i++) {
                 sum += sudoku.get(row).get(i);
             }
             if (sum != VALID_SUM) {
+                errors.add(1); // count error
                 System.out.println("Invalid Solution @ row " + (row + 1) + ".");
             }
         }
@@ -79,22 +78,20 @@ public class SudokuValidator {
     public static class Columns implements Runnable {
         private Vector<Vector<Integer>> sudoku; // The puzzle being tested
         private int column; // The column being tested
-
-        private Columns(int columnNumber, Vector<Vector<Integer>> puzzle) {
+        private Vector<Integer> errors; // Any errors caught
+        private Columns(int columnNumber, Vector<Vector<Integer>> puzzle, Vector<Integer> numErrors) {
             sudoku = puzzle;
             column = columnNumber;
+            errors = numErrors;
         }
-
         @Override
-        /** Override the run() method to tell the system
-         *  what the task to perform
-         */
         public void run() {
             int sum = 0;
             for (int i = 0; i < ROW_COLUMN_LENGTH; i++) {
                 sum += sudoku.get(i).get(column);
             }
             if (sum != VALID_SUM) {
+                errors.add(1); // count error
                 System.out.println("Invalid Solution @ column " + (column + 1) + ".");
             }
         }
@@ -104,15 +101,13 @@ public class SudokuValidator {
     public static class Squares implements Runnable {
         private Vector<Vector<Integer>> sudoku; // The puzzle being tested
         private int square; // The square being tested
-        public Squares(int squareNumber, Vector<Vector<Integer>> puzzle) {
+        private Vector<Integer> errors; // Any errors caught
+        public Squares(int squareNumber, Vector<Vector<Integer>> puzzle, Vector<Integer> numErrors) {
             sudoku = puzzle;
             square = squareNumber;
+            errors = numErrors;
         }
-
         @Override
-        /** Override the run() method to tell the system
-         *  what the task to perform
-         */
         public void run() {
         int sum = 0;
             for (int c = (square % 3)*3; c < (square % 3)*3 + SQUARE_LENGTH; c++) {
@@ -121,6 +116,7 @@ public class SudokuValidator {
                 }
             }
             if (sum != VALID_SUM) {
+                errors.add(1); // count error
                 System.out.println("Invalid Solution @ square " + (square + 1) + ".");
             }
         }
